6e60427e340b1ddfcdbc4be42fbc1af4ec350171
Gita Stičinska <s6_sticin_g@venta.lv>
2018-07-31 01:16:08 +0300

    added model class Comment

---
+++ src/main/java/com/interviewtask/blog/models/Comment.java
@@ -0,0 +1,61 @@
+package com.interviewtask.blog.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="comments")
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column
+	private String commenterName;
+	@Column(columnDefinition = "TEXT")	
+	private String content;
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private Date datePosted;
+	@ManyToOne
+	@JoinColumn(name = "post_id")
+	private Post post;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDatePosted() {
+		return datePosted;
+	}
+	public void setDatePosted(Date datePosted) {
+		this.datePosted = datePosted;
+	}
+	public String getCommenterName() {
+		return commenterName;
+	}
+	public void setCommenterName(String commenterName) {
+		this.commenterName = commenterName;
+	}
+	
+	
+
+}
