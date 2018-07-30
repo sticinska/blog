09e88a3f1f6cb5985259bc1ce74899e85dbb7ffe
Gita Stičinska <s6_sticin_g@venta.lv>
2018-07-31 01:51:40 +0300

    changes in postcontroller - saving,reading post of database

--- src/main/java/com/interviewtask/blog/controllers/PostController.java
+++ src/main/java/com/interviewtask/blog/controllers/PostController.java
@@ -3,8 +3,12 @@ package com.interviewtask.blog.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.interviewtask.blog.models.Post;
 import com.interviewtask.blog.repositories.PostRepository;
 
 @Controller
@@ -19,5 +23,30 @@ public class PostController {
 		return "index";
 	}
 	
+	@RequestMapping("/posts/{postID}")
+	public String getMovieById(@PathVariable(value = "postID") Long postID,
+			Model model) {
+		try {
+			model.addAttribute("post", postRepo.findById(postID).get());
+		}catch(Exception e){
+			return "index";
+		}
+		
+		
+		return "post";
+}
+	
+	@GetMapping("/admin/addPost")
+	public String addPostView(Model model) {
+		model.addAttribute("post", new Post());
+		return "admin/addpost";
+	}
+	
+	@PostMapping("/admin/addPost")
+	public String addPostFormProcess(Post post) {
+		postRepo.save(post);
+		return "redirect:/";
+	}
+
 	
 }
